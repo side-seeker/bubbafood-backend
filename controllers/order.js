c92b057131db08637823184faeded2301e9d4a12
gumgumNunchuks <850066882@genpact.com>
2023-05-09 23:53:19 +0530

    orders completed

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,6 @@
 const salesforce = require('../salesforce')
 const status = require('http-status')
+const chalk = require('chalk')
 
 /**
  * Get Orders for a Customer
@@ -14,24 +15,33 @@ async function createOrders(req, res) {
 
         // TODO: Payment Details Pending for now...
 
-        // Obtain the Result Order
-        const order = await salesforce.conn.sobject('Order__c').create({
-            User__c: userId,
-            Restraunt__c: restaurantId,
-            Takeaway_Delivery__c: deliveryOption
-        }, (err, ret) => {
-            if (err) {
-                console.log(err)
-                return res.status(status.INTERNAL_SERVER_ERROR).send()
-            }
-            console.log(ret)
-        })
+        // Obtain the Order
+        const order = await salesforce.conn.sobject('UpdatedOrder__c')
+            .create({
+                User__c: userId,
+                Restraunt__c: restaurantId,
+                Delivery_Takeaway__c: deliveryOption
+            }, (err, ret) => {
+                if (err) {
+                    console.log(err)
+                    return res.status(status.INTERNAL_SERVER_ERROR).send()
+                }
+                console.log(chalk.bold.greenBright('Order Created'))
+                console.log('Order ID:', chalk.bold(ret.id))
+            })
 
-        salesforce.conn.sobject('Order_Details__c')
-            .create(cartItems.map((item) => ({ Order__c: order.id, Food_Item__c: item.ID, Quantity: 1 })), (err, _ret) => {
+        // Reducing the cart to list of items
+        const orderDetails = Object.keys(cartItems)
+            .map(brand => cartItems[brand])
+            .reduce((prev, curr) => prev.concat(curr))
+
+        // Pushing Order details
+        salesforce.conn.sobject('Order_Detail__c')
+            .create(orderDetails.map((item) => ({ UpdatedOrder__c: order.id, Food_Item__c: item.Id })), (err, ret) => {
                 if (err) {
                     console.log(err)
                 }
+                ret.forEach((entry) => entry.success ? console.log(chalk.green('Inserted ID:'), entry.id): console.log(chalk.red('Failed to add Entry')))
             })
         return res.status(status.CREATED).send()
     }
