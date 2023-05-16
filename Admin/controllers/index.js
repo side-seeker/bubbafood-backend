ad1a4b01349b95e8dc2241b883e165bcd59ce487
JahnaviPasala <850066689@genpact.com>
2023-05-16 16:19:17 +0530

    commit

--- Admin/controllers/index.js
+++ Admin/controllers/index.js
@@ -2,11 +2,13 @@ const foodmanagement=require('./foodmanagement')
 const order=require('./orderComment')
 const reservation=require('./reservationComment')
 const user=require('./user')
+const ingredientsmanagement=require('./ingredientsmanagement')
 
 module.exports = {
     ...foodmanagement,
     ...order,
     ...reservation,
-    ...user
+    ...user,
+    ...ingredientsmanagement
 }
     
--- Admin/routes/index.js
+++ Admin/routes/index.js
@@ -2,7 +2,7 @@ const foodManagementRouter = require('./foodmanagement-rt')
 const orderCommentRouter=require('./orderComment-rt.js')
 const reservationCommentRouter=require('./reservationComment-rt')
 const userRouter=require('./user-rt.js')
-
+const ingredientsmanagementRouter=require('./ingredientsmanagement-rt')
 const express = require('express')
 
 // Index Router
@@ -11,4 +11,5 @@ router.use('/foodManagement', foodManagementRouter)
 router.use('/reservation',reservationCommentRouter)
 router.use('/order',orderCommentRouter)
 router.use('/user',userRouter)
+router.use('/ingredientsmanagement',ingredientsmanagementRouter)
 module.exports = router
