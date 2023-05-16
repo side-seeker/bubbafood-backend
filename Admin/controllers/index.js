41842fda3af8d5e61976c4781bf00de5f1bb06da
JahnaviPasala <850066689@genpact.com>
2023-05-16 16:13:10 +0530

    commit

--- Admin/controllers/index.js
+++ Admin/controllers/index.js
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
 const foodmanagement=require('./foodmanagement')
+const order=require('./orderComment')
+const reservation=require('./reservationComment')
+const user=require('./user')
 
 module.exports = {
-    ...foodmanagement
-}
-    
-=======
-const order = require('./orderComment')
-const reservation = require('./reservationComment')
-const user = require('./user')
-
-
-module.exports = {
+    ...foodmanagement,
     ...order,
     ...reservation,
     ...user
 }
->>>>>>> d24293e4698e54d7706f4c833e87467335be4251
+    
--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const router = require('./routes')
 const adRouter = require('./Ads/')
-<<<<<<< HEAD
 const admin= require('./Admin/routes')
-=======
-const adminRouter = require('./Admin/routes')
->>>>>>> d24293e4698e54d7706f4c833e87467335be4251
 const express = require('express')
 const cors = require('cors')
 const { conn } = require('./salesforce')
@@ -19,11 +15,7 @@ app.use(express.json())
 app.use(logger)
 app.use('/', router)
 app.use('/ads', adRouter)
-<<<<<<< HEAD
 app.use('/admin', admin)
-=======
-app.use('/admin', adminRouter)
->>>>>>> d24293e4698e54d7706f4c833e87467335be4251
 
 try {
     conn.login(process.env.SALESFORCE_USERNAME, process.env.SALESFORCE_PWD)
